d6fe242b3d182fc475644701267e00939221e770
RGijsbers <90719394+RGijsbers@users.noreply.github.com>
2023-05-18 10:24:03 +0200

    Q-learning works in the agent file

--- A4Agents.py
+++ A4Agents.py
@@ -81,101 +81,61 @@ class A2CCritic:
 
 
 class A2CAgent:
-
-
     pass
 
 
 class QLearningAgent(object):
 
-    def __init__(self, n_actions, n_states, epsilon):
+    def __init__(self, n_states, n_actions, learning_rate, epsilon, gamma):
         self.n_actions = n_actions
         self.n_states = n_states
         self.epsilon = epsilon
-        self.q_list = [0] * n_states
+        self.learning_rate = learning_rate
+        self.gamma = gamma
+        self.Q_sa = np.zeros((self.n_states, self.n_actions))
     
-    def get_qvalue_possible_moves(self, state, possible_actions, q_array):
-        possible_actions_qvalue_list = [0] * len(possible_actions)
-        y_location = int(state/12) #get 2D location from the 1D state
-        x_location = state%12
+    # def get_qvalue_possible_moves(self, state, possible_actions, q_array):
+    #     possible_actions_qvalue_list = [0] * len(possible_actions)
+    #     y_location = int(state/12) #get 2D location from the 1D state
+    #     x_location = state%12
 
-        for action in possible_actions:
-            if action == 0:
-                if y_location == 0:
-                    possible_actions_qvalue_list[action] = -100 #make sure the algorithm doesn't try to walk off the board
-                else:
-                    possible_actions_qvalue_list[action] = q_array[y_location-1][x_location]
-            if action == 1:
-                if y_location == 11:
-                    possible_actions_qvalue_list[action] = -100 #make sure the algorithm doesn't try to walk off the board
-                else:
-                    possible_actions_qvalue_list[action] = q_array[y_location+1][x_location]
-            if action == 2:
-                if x_location == 0:
-                    possible_actions_qvalue_list[action] = -100 #make sure the algorithm doesn't try to walk off the board
-                else:
-                    possible_actions_qvalue_list[action] = q_array[y_location][x_location-1]
-            if action == 3:
-                if x_location == 11:
-                    possible_actions_qvalue_list[action] = -100 #make sure the algorithm doesn't try to walk off the board
-                else:
-                    possible_actions_qvalue_list[action] = q_array[y_location][x_location+1]
+    #     for action in possible_actions:
+    #         if action == 0:
+    #             if y_location == 0:
+    #                 possible_actions_qvalue_list[action] = -100 #make sure the algorithm doesn't try to walk off the board
+    #             else:
+    #                 possible_actions_qvalue_list[action] = q_array[y_location-1][x_location]
+    #         if action == 1:
+    #             if y_location == 11:
+    #                 possible_actions_qvalue_list[action] = -100 #make sure the algorithm doesn't try to walk off the board
+    #             else:
+    #                 possible_actions_qvalue_list[action] = q_array[y_location+1][x_location]
+    #         if action == 2:
+    #             if x_location == 0:
+    #                 possible_actions_qvalue_list[action] = -100 #make sure the algorithm doesn't try to walk off the board
+    #             else:
+    #                 possible_actions_qvalue_list[action] = q_array[y_location][x_location-1]
+    #         if action == 3:
+    #             if x_location == 11:
+    #                 possible_actions_qvalue_list[action] = -100 #make sure the algorithm doesn't try to walk off the board
+    #             else:
+    #                 possible_actions_qvalue_list[action] = q_array[y_location][x_location+1]
 
-        return possible_actions_qvalue_list
+    #     return possible_actions_qvalue_list
     
-    def select_action(self, possible_actions, q_values_possible_actions):
-        q_value_action_list = [0] * len(possible_actions)
+    def select_action(self, s, epsilon):
+        highest_sa = max(self.Q_sa[s]) #the best action according to the Q table
+        smart_action = np.where(self.Q_sa[s] == highest_sa)[0][0] #get the right action number out of it
 
-        for action in possible_actions:
-            q_value_action_list[action] = 0
-
-        highest_reward = max(q_values_possible_actions) #get the 'best' move from all the possible moves
-        index = q_values_possible_actions.index(highest_reward)
-
-        random_number = random.randrange(0, 100, 1) #get the random chance of doing a random move
-        if random_number > self.epsilon*100: #if not the case do the 'best' move found
-            a = index            
+        if np.random.rand() > epsilon: #do the smart action
+            a = smart_action
         else:
-            a = random.choice(range(self.n_actions)) #otherwhise do a random move
+            a = np.random.randint(0,self.n_actions) #do a random action
         return a
 
-    def cliff_update(self, state, action, q_array):
-        y_location = int(state/12) #get the 2D location from the 1D state
-        x_location = state%12
-
-        if action == 0: #make sure you have the right coordinate in the q_array in order to make that vale a cliff
-            if y_location == 0:
-                pass
-            else:
-                y_location -= 1
-        if action == 1:
-           if y_location == 11:
-               pass
-           else:
-                y_location += 1
-        if action == 2:
-            if x_location == 0:
-                pass
-            else:
-                x_location -= 1
-        if action == 3:
-            if x_location == 11:
-                pass
-            else:
-                x_location += 1
-        q_array[y_location][x_location] = -math.inf #give the cliff a value so that it won't get chosen again
-        pass
-
-    def update_qlearning(self, state, next_state, possible_actions, reward, alpha, gamma, q_array):
-        y_location = int(state/12) #get the 2D location from the 1D state
-        x_location = state%12
-      
-        
-        best_next_move = max(QLearningAgent.get_qvalue_possible_moves(self, state=next_state, possible_actions=possible_actions, q_array=q_array))
-
-        q_array[y_location][x_location] = q_array[y_location][x_location] + alpha*(reward + gamma*best_next_move - q_array[y_location][x_location]) #update the q_array so that the algorithm learns from it's moves
-
-        state = next_state
+    def update(self,s,a,r,done,s_next,n_planning_updates):
+        self.Q_sa[s][a] = self.Q_sa[s][a] + self.learning_rate*(r + self.gamma*(self.gamma*max(self.Q_sa[s_next])) - self.Q_sa[s][a])
+        s = s_next
         pass
 
 def test():
@@ -184,13 +144,13 @@ def test():
     gamma = 0.99
 
     # Algorithm parameters
-    policy = 'dyna' # 'A2C' 
+    policy =  'Q' # 'dyna' 'A2C' 
     epsilon = 0.1
     learning_rate = 0.5
     n_planning_updates = 5
 
     # Plotting parameters
-    plot = False
+    plot = True
     plot_optimal_policy = True
     step_pause = 0.0001
     
@@ -199,7 +159,9 @@ def test():
     if policy == 'dyna':
         pi = DynaAgent(env.n_states,env.n_actions,learning_rate,gamma) # Initialize Dyna policy
     elif policy == 'A2C':    
-        pi = A2C(env.n_states,env.n_actions,learning_rate,gamma) # Initialize A2C policy
+        pi = A2CActor(env.n_states,env.n_actions,learning_rate,gamma) # Initialize A2C policy
+    elif policy == 'Q':
+        pi = QLearningAgent(env.n_states,env.n_actions,learning_rate,epsilon,gamma) # Initialize Q-learning policy
     else:
         raise KeyError('Policy {} not implemented'.format(policy))
     
